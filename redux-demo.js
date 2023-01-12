b68561ad694523a0261eb0ceb370162323a10652
sosdan73 <daniel.sosinskiy@gmail.com>
2023-01-12 17:44:35 +0200

    adds a decrement dispatch

--- redux-demo.js
+++ redux-demo.js
@@ -6,6 +6,11 @@ const counterReducer = (state = {counter: 0}, action) => {
 			counter: state.counter + 1,
 		}
 	}
+	if (action.type === 'decrement') {
+		return {
+			counter: state.counter - 1,
+		}
+	}
 	return state;
 }
 
@@ -19,3 +24,4 @@ const counterSubscriber = _ => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'decrement' })
